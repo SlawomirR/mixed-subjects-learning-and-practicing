e8263e15d404941f792abd7eeda9beb34f95adf4
SlawomirR <no.for.spam@wp.pl>
2018-08-20 20:31:39 +0200

    Task implementation.

--- decode-formatted-string/src/DecodeFormattedString.java
+++ decode-formatted-string/src/DecodeFormattedString.java
@@ -6,5 +6,47 @@
  * Jeśli otrzymany String jest pusty, metoda zwraca pustą Mapę.
  */
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DecodeFormattedString {
+
+    private static final String STRING_TO_DECODE = "=&=wartość1&klucz2=&klucz3=wartosc3&=wartosc4&klucz5=&=";
+
+    public static void main(String[] args) {
+        System.out.println("===> Zadanie 2: " + decode(STRING_TO_DECODE).toString());
+    }
+
+    static Map<String, String> decode(String s) {
+        Map<String, String> stringMap = new HashMap<>();
+        if (s == null) {
+            return null;
+        }
+        if (s.isEmpty()) {
+            return new HashMap<>();
+        }
+        if (!s.contains("=")) {
+            throw new IllegalArgumentException("String must contain '=' sign!");
+        }
+        String[] strings = s.split("&");
+        for (String str : strings) {
+            String[] splitOnEqualSign = str.split("=");
+            if (splitOnEqualSign.length == 0) {
+                continue;
+            }
+            if (splitOnEqualSign.length > 2) {
+                throw new IllegalArgumentException("String must contain single '=' signs separated by '&' sign.");
+            }
+            if (stringMap.containsKey(splitOnEqualSign[0])) {
+                System.err.println("WARNING: There were duplicated key! So value was updated to the last occurrence!");
+            }
+            if (splitOnEqualSign.length == 1) {
+                stringMap.put(splitOnEqualSign[0], "");
+            }
+            if (splitOnEqualSign.length == 2) {
+                stringMap.put(splitOnEqualSign[0], splitOnEqualSign[1]);
+            }
+        }
+        return stringMap;
+    }
 }
