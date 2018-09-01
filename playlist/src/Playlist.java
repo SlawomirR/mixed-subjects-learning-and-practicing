6101abcdfafac81fd56f760707ee92f75b655f0f
SlawomirR <no.for.spam@wp.pl>
2018-09-02 00:21:49 +0200

    Implemented isRepeatingPlaylist() method in playlist module. Closes #2

--- playlist/src/Playlist.java
+++ playlist/src/Playlist.java
@@ -1,5 +1,7 @@
 public class Playlist {
+
     public static class Song {
+
         private String name;
         private Song nextSong;
 
@@ -7,12 +9,31 @@ public class Playlist {
             this.name = name;
         }
 
-        public void setNextSong(Song nextSong) {
+        void setNextSong(Song nextSong) {
             this.nextSong = nextSong;
         }
 
-        public boolean isRepeatingPlaylist() {
-            throw new UnsupportedOperationException("Waiting to be implemented.");
+        boolean isRepeatingPlaylist() {
+            Song fast;
+            Song slow;
+            if (Song.this.nextSong == null) {
+                return false;
+            }
+            slow = fast = Song.this.nextSong;
+            while (true) {
+                slow = slow.nextSong;
+                if (fast.nextSong != null) {
+                    fast = fast.nextSong.nextSong;
+                } else {
+                    return false;
+                }
+                if (slow == null || fast == null) {
+                    return false;
+                }
+                if (slow == fast) {
+                    return true;
+                }
+            }
         }
     }
 
@@ -26,3 +47,7 @@ public class Playlist {
         System.out.println(first.isRepeatingPlaylist());
     }
 }
+
+// links to web sites that help:
+// https://www.cs.cmu.edu/~adamchik/15-121/lectures/Linked%20Lists/linked%20lists.html
+// https://stackoverflow.com/questions/2663115/how-to-detect-a-loop-in-a-linked-list
