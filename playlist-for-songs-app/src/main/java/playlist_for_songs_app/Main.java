6b339398833962f016a681dfd0d8dd15b618abdc
SlawomirR <no.for.spam@wp.pl>
2018-10-03 21:04:44 +0200

    Playlist for songs: initially implemented main method.

--- playlist-for-songs-app/src/main/java/playlist_for_songs_app/Main.java
+++ playlist-for-songs-app/src/main/java/playlist_for_songs_app/Main.java
@@ -1,8 +1,52 @@
 package playlist_for_songs_app;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
 public class Main {
 
-    public static void main(String[] args) {
+    private static List<Album> albums = new ArrayList<>();
 
+    public static void main(String[] args) {
+        Album album = new Album("First Album", "First Artist");
+        album.addSong("TitleA1S1", 2.34);
+        album.addSong("TitleA1S2", 1.43);
+        album.addSong("TitleA1S3", 3.24);
+        album.addSong("TitleA1S4", 2.54);
+        album.addSong("TitleA1S5", 4.03);
+        album.addSong("TitleA1S6", 1.56);
+        albums.add(album);
+
+        album = new Album("Second Album", "Second Artist");
+        album.addSong("TitleA2S1", 4.34);
+        album.addSong("TitleA2S2", 4.43);
+        album.addSong("TitleA2S3", 2.24);
+        album.addSong("TitleA2S4", 1.54);
+        album.addSong("TitleA2S5", 3.03);
+        album.addSong("TitleA2S6", 2.56);
+        albums.add(album);
+
+        List<Song> playlist = new LinkedList<>();
+        albums.get(0).addToPlaylist("TitleA1S4", playlist);
+        albums.get(0).addToPlaylist("TitleA1S2", playlist);
+        albums.get(0).addToPlaylist("TitleA1S14", playlist); // Dose not exist
+        albums.get(0).addToPlaylist(6, playlist);
+        albums.get(1).addToPlaylist(6, playlist);
+        albums.get(1).addToPlaylist(3, playlist);
+        albums.get(1).addToPlaylist(2, playlist);
+        albums.get(1).addToPlaylist(24, playlist); // There is no such track
+
+        play(playlist);
+    }
+
+    private static void play(List<Song> playlist) {
+        ListIterator<Song> listIterator = playlist.listIterator();
+        if (playlist.size() == 0) {
+            System.out.println("No songs in playlist");
+        } else {
+            System.out.println("Now playing " + listIterator.next());
+        }
     }
 }
