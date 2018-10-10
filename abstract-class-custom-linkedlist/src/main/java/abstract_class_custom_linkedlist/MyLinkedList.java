422e37a6ca2f36831f6ad545e444d4166d9a3bac
SlawomirR <no.for.spam@wp.pl>
2018-10-10 16:57:52 +0200

    Abstract class custom linkedList: implemented addItem method of MyLinkedList class

--- abstract-class-custom-linkedlist/src/main/java/abstract_class_custom_linkedlist/MyLinkedList.java
+++ abstract-class-custom-linkedlist/src/main/java/abstract_class_custom_linkedlist/MyLinkedList.java
@@ -14,7 +14,41 @@ public class MyLinkedList implements NodeList {
     }
 
     @Override
-    public boolean addItem(ListItem item) {
+    public boolean addItem(ListItem newItem) {
+        if (this.root == null) {
+            // This list was empty so this item becomes the head of the list
+            this.root = newItem;
+            return true;
+        }
+        ListItem currentItem = this.root;
+        while (currentItem != null) {
+            int comparison = currentItem.compareTo(newItem);
+            if (comparison < 0) {
+                // newItem is greater so move right if possible
+                if (currentItem.next() != null) {
+                    currentItem = currentItem.next();
+                } else {
+                    // there is no next so insert at the end of the list
+                    currentItem.setNext(newItem).setPrevious(currentItem);
+                    return true;
+                }
+            } else if (comparison > 0) {
+                // newItem is smaller so move left
+                if (currentItem.previous() != null) {
+                    currentItem.previous().setNext(newItem).setPrevious(currentItem.previous());
+                    newItem.setNext(currentItem).setPrevious(newItem);
+                } else {
+                    // the node with the previous is the root
+                    newItem.setNext(this.root).setPrevious(newItem);
+                    this.root = newItem;
+                }
+                return true;
+            } else {
+                // equal
+                System.out.println(newItem.getValue() + " is already present, not added");
+                return false;
+            }
+        }
         return false;
     }
 
