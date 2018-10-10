9c641c0169eab074b1553472e45cf702e9e0c23b
SlawomirR <no.for.spam@wp.pl>
2018-10-10 16:54:46 +0200

    Abstract class custom linkedList: created MyLinkedList class template

---
+++ abstract-class-custom-linkedlist/src/main/java/abstract_class_custom_linkedlist/MyLinkedList.java
@@ -0,0 +1,30 @@
+package abstract_class_custom_linkedlist;
+
+public class MyLinkedList implements NodeList {
+
+    private ListItem root = null;
+
+    public MyLinkedList(ListItem root) {
+        this.root = root;
+    }
+
+    @Override
+    public ListItem getRoot() {
+        return this.root;
+    }
+
+    @Override
+    public boolean addItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public boolean removeItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public void traverse(ListItem root) {
+
+    }
+}
