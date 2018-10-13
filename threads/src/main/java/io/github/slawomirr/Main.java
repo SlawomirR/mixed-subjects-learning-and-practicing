297e5fab47ca2249e83e30a8f51f1a2cdad85d18
SlawomirR <no.for.spam@wp.pl>
2018-10-13 10:16:09 +0200

    Threads: interrupt and join

--- threads/src/main/java/io/github/slawomirr/Main.java
+++ threads/src/main/java/io/github/slawomirr/Main.java
@@ -1,5 +1,7 @@
 package io.github.slawomirr;
 
+import static io.github.slawomirr.ThreadColorIntellijConsole.ANSI_RED;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,21 +11,25 @@ public class Main {
         anotherThread.setName("== Another Thread ==");
         anotherThread.start();
 
-        new Thread() {
-
-            @Override
-            public void run() {
-                System.out.println(ThreadColorIntellijConsole.ANSI_GREEN + "Hello from the anonymous class thread.");
-            }
-        }.start();
+        new Thread(() -> System.out.println(ThreadColorIntellijConsole.ANSI_GREEN + "Hello from the anonymous class thread."))
+                .start();
 
         Thread myRunnableThread = new Thread(new MyRunnable() {
             @Override
             public void run() {
-                System.out.println(ThreadColorIntellijConsole.ANSI_RED + "Hello form the anonymous class's implementation of run().");
+                System.out.println(ANSI_RED + "Hello form the anonymous class's implementation of run().");
+                try {
+//                    anotherThread.join();
+//                    System.out.println(ANSI_RED + "AnotherThread terminated, so I'm running again.");
+                    anotherThread.join(2_000);
+                    System.out.println(ANSI_RED + "AnotherThread terminated, or 2s timed out, so I'm running again.");
+                } catch (InterruptedException e) {
+                    System.out.println(ANSI_RED + "I couldn't wait after all. I was interrupted.");
+                }
             }
         });
         myRunnableThread.start();
+//        anotherThread.interrupt();
 
         System.out.println(ThreadColorIntellijConsole.ANSI_PURPLE + "Hello again from the main thread.");
     }
