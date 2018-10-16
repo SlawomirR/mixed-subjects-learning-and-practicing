972db51f242487e836fcc85b9776d111267baaf1
SlawomirR <no.for.spam@wp.pl>
2018-10-16 15:20:21 +0200

    Threads: live lock challenge, the code need to be fixed

--- threads/multithread-bank-account-demo/src/main/java/io/github/slawomirr/BankAccount.java
+++ threads/multithread-bank-account-demo/src/main/java/io/github/slawomirr/BankAccount.java
@@ -25,31 +25,41 @@ class BankAccount {
 //    }
 
     public void deposit(double amount) {
+        boolean status = false;
         try {
             if (lock.tryLock(1_000, TimeUnit.MILLISECONDS)) {
                 try {
                     balance += amount;
+                    status = true;
                 } finally {
                     lock.unlock();
                 }
+            } else {
+                System.out.println(Main.ANSI_RESET + "Could not get the lock! (deposit)");
             }
         } catch (InterruptedException e) {
-            System.out.println(Main.ANSI_RED + "Could not get the lock! (deposit)");
+            System.out.println(Main.ANSI_RED + "Deposit thread interrupted");
         }
+        System.out.println(Main.ANSI_RESET + "Transaction status = " + status);
     }
 
     public void withdraw(double amount) {
+        boolean status = false;
         try {
             if (lock.tryLock(1_000, TimeUnit.MILLISECONDS)) {
                 try {
                     balance -= amount;
+                    status = true;
                 } finally {
                     lock.unlock();
                 }
+            } else {
+                System.out.println("Could not get the lock! (withdraw)");
             }
         } catch (InterruptedException e) {
-            System.out.println(Main.ANSI_RED + "Could not get the lock! (withdraw)");
+            System.out.println(Main.ANSI_RED + "Withdraw thread interrupted");
         }
+        System.out.println(Main.ANSI_RESET + "Transaction status = " + status);
     }
 
     public double getBalance() {
