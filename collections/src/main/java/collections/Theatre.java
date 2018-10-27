d777fba0fcd2211834d0b3f7fe594a773ac343b1
SlawomirR <no.for.spam@wp.pl>
2018-10-27 16:04:47 +0200

    Collections: applied binary search

--- collections/src/main/java/collections/Theatre.java
+++ collections/src/main/java/collections/Theatre.java
@@ -8,7 +8,7 @@ public class Theatre {
     private final String theatreName;
     private List<Seat> seats = new ArrayList<>();
 
-    public Theatre(String theatreName, int numRows, int seatsPerRow) {
+    Theatre(String theatreName, int numRows, int seatsPerRow) {
         this.theatreName = theatreName;
 
         int lastRow = 'A' + (numRows - 1);
@@ -24,19 +24,35 @@ public class Theatre {
         return theatreName;
     }
 
-    public boolean reserveSeat(String seatNumber) {
-        Seat requestedSeat = null;
-        for (Seat seat : seats) {
-            if (seat.getSeatNumber().equals(seatNumber)) {
-                requestedSeat = seat;
-                break;
-            }
-        }
-        if (requestedSeat == null) {
+    boolean reserveSeat(String seatNumber) {
+/*
+        Seat requestedSeat = new Seat(seatNumber);
+        int foundSeat = Collections.binarySearch(seats, requestedSeat, null);
+        if (foundSeat >= 0) {
+            return seats.get(foundSeat).reserve();
+        } else {
             System.out.println("There is no seat " + seatNumber);
             return false;
         }
-        return requestedSeat.reserve();
+*/
+        int low = 0;
+        int high = seats.size() - 1;
+
+        while (low <= high) {
+            System.out.print(".");
+            int mid = (low + high) / 2;
+            Seat midVal = seats.get(mid);
+            int cmp = midVal.getSeatNumber().compareTo(seatNumber);
+
+            if (cmp < 0)
+                low = mid + 1;
+            else if (cmp > 0)
+                high = mid - 1;
+            else
+                return seats.get(mid).reserve();
+        }
+        System.out.println("There is no seat " + seatNumber);
+        return false;
     }
 
     // for testing
@@ -46,16 +62,21 @@ public class Theatre {
         }
     }
 
-    private class Seat {
+    private class Seat implements Comparable<Seat> {
 
         private final String seatNumber;
         private boolean reserved = false;
 
-        public Seat(String seatNumber) {
+        Seat(String seatNumber) {
             this.seatNumber = seatNumber;
         }
 
-        public boolean reserve() {
+        @Override
+        public int compareTo(Seat seat) {
+            return this.getSeatNumber().compareToIgnoreCase(seat.getSeatNumber());
+        }
+
+        boolean reserve() {
             if (!this.reserved) {
                 this.reserved = true;
                 System.out.println("Seat " + seatNumber + " reserved.");
@@ -75,7 +96,7 @@ public class Theatre {
             }
         }
 
-        public String getSeatNumber() {
+        String getSeatNumber() {
             return seatNumber;
         }
     }
