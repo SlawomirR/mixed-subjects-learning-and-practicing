a385c9060bf8e337465c2e7bc96dd0c9618a7d83
SlawomirR <no.for.spam@wp.pl>
2018-10-29 21:48:53 +0100

    TDD example: another few steps was done

--- tdd-example-WyCash-multicurrency/src/main/java/wycash_multicurrency_demo/Dollar.java
+++ tdd-example-WyCash-multicurrency/src/main/java/wycash_multicurrency_demo/Dollar.java
@@ -1,19 +1,12 @@
 package wycash_multicurrency_demo;
 
-class Dollar {
+class Dollar extends Money {
 
-    private int amount;
-
-    Dollar(int amount) {
-        this.amount = amount;
+    Dollar(int amount, String currency) {
+        super(amount, currency);
     }
 
-    Dollar times(int multiplier) {
-        return new Dollar(amount * multiplier);
-    }
-
-    public boolean equals(Object object) {
-        Dollar dollar = (Dollar) object;
-        return amount == dollar.amount;
+    Money times(int multiplier) {
+        return Money.dollar(amount * multiplier);
     }
 }
--- tdd-example-WyCash-multicurrency/src/main/java/wycash_multicurrency_demo/Franc.java
+++ tdd-example-WyCash-multicurrency/src/main/java/wycash_multicurrency_demo/Franc.java
@@ -1,19 +1,12 @@
 package wycash_multicurrency_demo;
 
-class Franc {
+class Franc extends Money {
 
-    private int amount;
-
-    Franc(int amount) {
-        this.amount = amount;
+    Franc(int amount, String currency) {
+        super(amount, currency);
     }
 
-    Franc times(int multiplier) {
-        return new Franc(amount * multiplier);
-    }
-
-    public boolean equals(Object object) {
-        Franc dollar = (Franc) object;
-        return amount == dollar.amount;
+    Money times(int multiplier) {
+        return Money.franc(amount * multiplier);
     }
 }
