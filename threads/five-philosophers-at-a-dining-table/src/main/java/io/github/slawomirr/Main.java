f3afbd0a7cc085ba261e60998550e6f23597db06
SlawomirR <no.for.spam@wp.pl>
2018-11-02 22:59:32 +0100

    Threads: five philosopher multi-thread problem, initial commit

---
+++ threads/five-philosophers-at-a-dining-table/src/main/java/io/github/slawomirr/Main.java
@@ -0,0 +1,16 @@
+package io.github.slawomirr;
+
+/*
+ * The problem goes like this: five philosophers spend all of their time either thinking or eating. They sit around
+ * a circular table with five chairs and five forks. There are only five forks and all five philosophers need to pick
+ * the two nearest (one from his left and one from his right) forks to eat. Once a philosopher finishes eating, he
+ * puts down both forks and starts thinking. A philosopher cannot pick up a fork if his neighbor is using it. What
+ * happens if each of the five philosophers picks up one fork from his right and waits for his left fork to be
+ * released by his neighbor?
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
